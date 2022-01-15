84fb014ad196272adf5278f202c5127c5b1b1e2b
amanbop <aman_boparai@yahoo.com>
2022-01-14 21:24:04 -0800

    Intial commit using bundler gem structure

---
+++ lib/reparse.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "reparse/version"
+
+module Reparse
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/reparse/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Reparse
+  VERSION = "0.1.0"
+end
